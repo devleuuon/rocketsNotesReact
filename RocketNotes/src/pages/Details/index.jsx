baac93bbc6abd82c6e405c47a818cdefee8fb5d0
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-03-11 13:49:53 -0300

    section created

--- RocketNotes/src/pages/Details/index.jsx
+++ RocketNotes/src/pages/Details/index.jsx
@@ -1,5 +1,7 @@
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
+
 import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
 import { Button } from "../../components/Button"
 
 export function Details() {
@@ -7,6 +9,18 @@ export function Details() {
 return (
     <Container> 
     <Header />
+
+    <Section title="Links Úteis">
+        <Links>
+            <li>
+                <a href="https://rocketseat.com.br">https://rocketseat.com.br</a>
+            </li>
+            <li>
+                <a href="https://rocketseat.com.br">https://rocketseat.com.br</a>
+            </li>
+        </Links>
+    </Section>
+
     <Button title="Voltar" />
     </Container>
 )
