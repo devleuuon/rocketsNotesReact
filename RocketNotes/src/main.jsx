aeddbc2325d2a2d542b156976749f379978ef161
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-03-26 14:52:39 -0300

    SignIn created

--- RocketNotes/src/main.jsx
+++ RocketNotes/src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './pages/Styles/global'
-import theme from './pages/Styles/theme'
-import { Home } from './pages/Home'
+import GlobalStyles from './Styles/global.js'
+import theme from './Styles/theme.js'
+// import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Home />
+    <SignIn />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
